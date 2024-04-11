9d9484e319cbd95867934a1699ad7b6527c69687
Cameron Lund <cameronlund4@gmail.com>
2024-04-11 14:00:53 -0700

    More rediculousness

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
 echo "Hello world from super-duper-octo-waddle!"
+echo "Waddling hard to test .pt"
+for i in {1..10000}
+do
+  echo "super-duper-octo-waddle!\n" >> output.pt
+done
+echo "Done waddling!"
